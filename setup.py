33fbd2f7b9d0611fcbfc0e612bbe5ee7a8a240c3
nephlm <github@quasisemi.com>
2025-03-11 19:13:22 -0400

    I don't they were changes on my local file, seem to focus around packaging.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="tiddlywiki_parser",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["beautifulsoup4>=4.8.0", "requests>=2.22.0"],
+    entry_points={"console_scrips": ["tiddlywiki_parser = tiddlywiki_parser:main"]},
 )
